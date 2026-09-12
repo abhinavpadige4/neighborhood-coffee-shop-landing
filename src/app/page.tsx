e8ff664d1b2edb3532d579ee6bff42eab39d1036
abhinav <abhinavpadige06@gmail.com>
2026-09-12 20:11:11 +0530

    feat: add comment to trigger rebuild

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white dark:bg-gray-900">
+    <main className=\"min-h-screen bg-white dark:bg-gray-900\">
       <Header />
       <Hero />
       <Menu />
